f863d8a0f8deb1dc4a1aec8bc6f66160024ffc02
lukashanzl <wi20b103@technikum-wien.at>
2023-06-14 19:28:23 +0200

    initial commit

---
+++ src/main/java/com/example/slm_monitoring/SlmMonitoringApplication.java
@@ -0,0 +1,13 @@
+package com.example.slm_monitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlmMonitoringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlmMonitoringApplication.class, args);
+    }
+
+}
